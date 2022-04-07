00daeb4683c217c83042bf54ff0975f18b0163eb
pravallika <pravallika4java@gmail.com>
2022-04-07 07:18:44 -0400

    fixed JSON parsing

--- cookierecipe/src/main/java/com/pravallika/ge/cookierecipe/api/RecipeParserController.java
+++ cookierecipe/src/main/java/com/pravallika/ge/cookierecipe/api/RecipeParserController.java
@@ -1,6 +1,8 @@
 package com.pravallika.ge.cookierecipe.api;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.json.JSONObject;
 import org.jsoup.Jsoup;
@@ -16,36 +18,41 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RecipeParserController {
-	
+
 	public static final Logger logger = LoggerFactory.getLogger(RecipeParserController.class);
-			
-	
+
 	@PostMapping("/getRecipe")
-	public String getRecipe(@RequestParam String url){
-		
+	public String getRecipe(@RequestParam String url) {
+
 		Elements ingredientsList = null;
 		Elements recipeSteps = null;
 		JSONObject jsonResult = new JSONObject();
+		List<String> ingredients = new ArrayList<String>();
+		List<String> instructions = new ArrayList<String>();
 		try {
-			
+
 			Document document = Jsoup.connect(url).post();
 			ingredientsList = document.getElementsByClass("list--bullets");
-			for(Element ingredient: ingredientsList) {
-				Elements liValue = ingredient.getElementsByTag("li");
-				logger.info("element value {}", liValue);
-			}
+			//iterating through list using Lambda
+			ingredientsList.forEach(in -> {
+				ingredients.add(in.text());
+			});
+			jsonResult.put("Ingredients", ingredients);
+			
+			//using java 8 lambdas
 			recipeSteps = document.getElementsByClass("recipe__instructions");
-			
-			
+			recipeSteps.forEach(in -> {
+				instructions.add(in.text());
+			});
+			jsonResult.put("Instructions", instructions);
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			logger.error("caught Exception while fetching receipe details from {}", url);
 		}
-		
-		
-		String result = ingredientsList.toString() + recipeSteps.toString();
-		return result;
-		
-	}
 
+		//String result = ingredientsList.toString() + recipeSteps.toString();
+		return jsonResult.toString();
+
+	}
 }
